0d57b70f575638d33c91ba766e38bd659683a84c
Andrei Balteanu <andrei.balteanu@dstny.com>
2025-08-28 17:31:49 +0300

    .

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,3 +1,20 @@
+import profileImage from "@/assets/images/profile-image.jpg";
+import Image from "next/image";
+
 export const HeroSection = () => {
-  return <div>Hero Section</div>;
+  return (
+    <div>
+      <div className="container">
+        <Image
+          className="rounded-full"
+          src={profileImage}
+          alt="profile image"
+        />
+        <div>
+          <div></div>
+          <div>Balteanu Andrei</div>
+        </div>
+      </div>
+    </div>
+  );
 };
