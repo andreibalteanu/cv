d6be77abb224192bfa44d2818aad2f755f19c550
Andrei Balteanu <andrei.balteanu@dstny.com>
2025-08-28 18:08:45 +0300

    styling

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,20 +1,33 @@
 import profileImage from "@/assets/images/profile-image.jpg";
 import Image from "next/image";
+import ArrowDown from "@/assets/icons/arrow-down.svg";
 
 export const HeroSection = () => {
   return (
-    <div>
-      <div className="container">
+    <div className="py-24">
+      <div className="container flex flex-col items-center">
         <Image
-          className="rounded-full"
+          className="rounded-full size-[150px]"
           src={profileImage}
           alt="profile image"
         />
-        <h1>Balteanu Andrei</h1>
+        <h1 className="font-serif text-3xl text-center mt-8 tracking-wide">
+          Balteanu Andrei
+        </h1>
         <p>
           Specialized in developing responsive web applications and engaging
           user interfaces using modern technologies.
         </p>
+        <div>
+          <button>
+            <span>Explore My Work</span>
+            <ArrowDown />
+          </button>
+          <button>
+            <span></span>
+            <span>Let's Connect</span>
+          </button>
+        </div>
       </div>
     </div>
   );
